9ab03393f2927d1f9f67199a8783ba141a79ff92
Jenno Ramcaj <jennoramcaj@gmail.com>
2026-08-03 08:51:08 +0200

    fix test

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -40,7 +40,7 @@ class HandleInertiaRequests extends Middleware
             ...parent::share($request),
             'name' => config('app.name'),
             'auth' => [
-                'user' => UserData::from($request->user()),
+                'user' => $request->user() ? UserData::from($request->user()) : null,
             ],
         ];
     }
