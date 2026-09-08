111e16161fe6e5682b8e86dc2adf625f52cdf730
emmanuele <emmanuele.locati@crslaghi.net>
2026-09-08 16:19:13 +0200

    aggiunta spatie/laravel-permission e seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,10 +17,15 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'first_name' => 'Test',
-            'last_name' => 'User',
-            'email' => 'test@example.com',
+        //        User::factory()->create([
+        //            'first_name' => 'Test',
+        //            'last_name' => 'User',
+        //            'email' => 'test@example.com',
+        //        ]);
+
+        $this->call([
+            RolesAndPermissionsSeeder::class,
         ]);
+
     }
 }
